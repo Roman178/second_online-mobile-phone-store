fbb020e111f47447e24f7d85c119b5d68755060f
Roman <ismagilov771@gmail.com>
2020-12-02 23:45:09 +0300

    Keep doing some CSS work...

--- src/components/cart/CheckoutPage.js
+++ src/components/cart/CheckoutPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { useState } from "react";
 import { Form, Input, Button, Select, Modal } from "antd";
--- src/components/common/DevicePage.js
+++ src/components/common/DevicePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Image } from "antd";
 import iphone11ProMaxImg from "../../img/iphone_11_pro_max.jpg";
 import ImageGallery from "react-image-gallery";
@@ -26,16 +26,42 @@ import "./DevicePage.css";
 //   },
 // ];
 
-// const []
-
 function DevicePage(props) {
   console.log(props);
+  const [cssClass, setCssClass] = useState("secondary-container-0");
+
+  function checkClassOnTheRight() {
+    switch (cssClass) {
+      case "secondary-container-0":
+        return setCssClass("secondary-container-500");
+      case "secondary-container-500":
+        return setCssClass("secondary-container-1000");
+      case "secondary-container-1000":
+        return setCssClass("secondary-container-1500");
+      default:
+        return undefined;
+    }
+  }
+
+  function checkClassOnTheLeft() {
+    switch (cssClass) {
+      case "secondary-container-1500":
+        return setCssClass("secondary-container-1000");
+      case "secondary-container-1000":
+        return setCssClass("secondary-container-500");
+      case "secondary-container-500":
+        return setCssClass("secondary-container-0");
+      default:
+        return undefined;
+    }
+  }
+
   return (
     <>
       <div className="container-product-slider">
         {/* <ImageGallery items={images} showNav={false} /> */}
 
-        <div className="secondary-container">
+        <div className={cssClass}>
           <div className="img-container">
             <img
               className="img-element"
@@ -51,18 +77,35 @@ function DevicePage(props) {
           <div className="img-container">
             <img
               className="img-element"
-              src="https://cdn.svyaznoy.ru/upload/iblock/53a/iphone_11_w_4.jpg/resize/483x483/hq/"
+              src="https://cdn.svyaznoy.ru/upload/iblock/48e/iphone_11_w_1.jpg/resize/483x483/hq/"
+            ></img>
+          </div>
+
+          <div className="img-container">
+            <img
+              className="img-element"
+              src="https://cdn.svyaznoy.ru/upload/iblock/db9/iphone_11_w_2.jpg/resize/483x483/hq/"
             ></img>
           </div>
         </div>
       </div>
-      <button
-        onClick={() => {
-          const el = document.getElementsByClassName("secondary-container");
-          el[0].style.transition = "transform 0.5s ease-in";
-          el[0].style.transform = "translateX(-500px)";
-        }}
-      ></button>
+      <button onClick={() => checkClassOnTheLeft()}>
+        {" "}
+        <img
+          width={50}
+          className="img-element"
+          src="https://cdn.svyaznoy.ru/upload/iblock/48e/iphone_11_w_1.jpg/resize/483x483/hq/"
+        ></img>
+      </button>
+
+      <button onClick={() => checkClassOnTheRight()}>
+        {" "}
+        <img
+          width={50}
+          className="img-element"
+          src="https://cdn.svyaznoy.ru/upload/iblock/db9/iphone_11_w_2.jpg/resize/483x483/hq/"
+        ></img>
+      </button>
     </>
   );
 }
