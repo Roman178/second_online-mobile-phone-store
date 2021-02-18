bc7a626b07d555303559bdef7c6b71a386b5a597
Roman <ismagilov771@gmail.com>
2021-02-18 23:35:26 +0300

    Styles.

--- src/components/common/NotFoundPage.js
+++ src/components/common/NotFoundPage.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-const NotFoundPage = () => <h>Page Not Found</h>;
+const NotFoundPage = () => (
+  <div className="not-found-cont">
+    <h1 className="not-found-cont-404">404</h1>
+    <h2 className="not-found-cont-h2">Page NOT FOUND! Sorry...</h2>
+  </div>
+);
 
 export default NotFoundPage;
