6e088bdcdeb1efd32e572c072f95f1c9c64d1269
Roman <ismagilov771@gmail.com>
2021-06-14 22:49:34 +0300

    README is updated.

---
+++ build/precache-manifest.c57c4b2a88a25bbd295e929695739421.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17e2e00c20ea8c14780391fa7582a7a",
+    "url": "/the-online-mobile-phone-store/index.html"
+  },
+  {
+    "revision": "b9d439fc9f900f6c158b",
+    "url": "/the-online-mobile-phone-store/static/css/2.bdc98c05.chunk.css"
+  },
+  {
+    "revision": "8b86e3d6d25d97f7bca3",
+    "url": "/the-online-mobile-phone-store/static/css/main.173e1bd6.chunk.css"
+  },
+  {
+    "revision": "b9d439fc9f900f6c158b",
+    "url": "/the-online-mobile-phone-store/static/js/2.0326e128.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/the-online-mobile-phone-store/static/js/2.0326e128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b86e3d6d25d97f7bca3",
+    "url": "/the-online-mobile-phone-store/static/js/main.c35e3e71.chunk.js"
+  },
+  {
+    "revision": "5ba409f637c5b825d111",
+    "url": "/the-online-mobile-phone-store/static/js/runtime-main.dcb556ab.js"
+  }
+]);
